7cc7cd4a080f23e24f756e4220d4b278a4993a4b
radluki <radluki@gmail.com>
2023-12-13 22:54:43 +0000

    Added todos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,9 +14,28 @@ export class AppController {
     return this.appService.getHello();
   }
   
+  // TODO: Add documentation
   @Get("films|species|vehicles|starships|people|planets")
   async default(@Req() request: Request): Promise<string> {
     this.logger.log('Default api proxy endpoint hit');
     return await this.cachedApiProxyService.get(request.url);
   }
+
+  //TODO: Add endpoints:
+  /*
+    Each film entry returned from the /films/ API has an opening_crawl property which
+    contains a short film plot description. Write an endpoint that will return:
+
+        A. an array of pairs of unique words from all films openings paired with their
+        number of occurrences in the text. Words should not be empty and should be
+        separated by space or any number of consecutive control characters (i.e
+        carriage return, line feed).
+        B. a name of a character from the /people API that appears the most often
+        across all of the openings of the film (or an array of names if there are more
+        characters with the same number). We are interested only in exact name
+        matches excluding eventual control characters in between when the name is
+        longer than a single word.
+  */
+
+  //TODO: GraphQL
 }
