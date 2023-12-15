a719fb53082de90e0c0a32ca43a952fbe7baf419
radluki <radluki@gmail.com>
2023-12-15 14:17:57 +0000

    people page arg type changed from string to number

--- src/accessGraphQl.sh
+++ src/accessGraphQl.sh
@@ -4,5 +4,5 @@ URL="http://app:3000/graphql"
 
 curl -X POST -H "Content-Type: application/json" \
     --data \
-        '{ "query": "{ people(name: \"Skywalker\") { count next results { name starships } } }" }' \
+        '{ "query": "{ people(page: 1) { count next results { name starships } } }" }' \
     $URL | jq
--- src/graphql.resolver.ts
+++ src/graphql.resolver.ts
@@ -1,15 +1,15 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { GraphqlService } from './graphql.service';
-import { People, Person } from './person.type';
+import { People } from './person.type';
 
-@Resolver((of) => People)
+@Resolver()
 export class GraphqlResolver {
   constructor(private readonly graphqlService: GraphqlService) {}
 
   @Query(() => People)
   async people(
     @Args('name', { type: () => String, nullable: true }) name?: string,
-    @Args('page', { type: () => String, nullable: true }) page?: string,
+    @Args('page', { type: () => Number, nullable: true }) page?: string,
   ) {
     return this.graphqlService.getPeople(name, page);
   }
