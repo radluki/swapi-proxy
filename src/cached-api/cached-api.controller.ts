503240448d08092630aaa1b6691ae149f0c392e4
radluki <radluki@gmail.com>
2024-01-01 15:20:56 +0000

    Renamed custom-cache.interceptor.ts to http-cache-connection-robust.interceptor.ts

--- src/cached-api/cached-api.controller.ts
+++ src/cached-api/cached-api.controller.ts
@@ -8,7 +8,7 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { createLogger } from '../utils/logger-factory';
-import { HttpCacheConectionRobustInterceptor } from '../utils/interceptors/custom-cache.interceptor';
+import { HttpCacheConectionRobustInterceptor } from '../utils/interceptors/http-cache-connection-robust.interceptor';
 import { ApiProxyService } from './api-proxy-service';
 import { JsonParseInterceptor } from '../utils/interceptors/json-parse.interceptor';
 
